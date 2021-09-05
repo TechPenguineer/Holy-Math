6b980642c534fe473ed95ead05ac72d16b9ef3b5
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-05 19:39:07 -0400

    .

--- HolyMath.py
+++ HolyMath.py
@@ -1,8 +1,9 @@
 from PyQt5 import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+import sys
 
-class app(QWidget):
+class App(QWidget):
     
     def __init__(self):
         super().__init__()
@@ -10,9 +11,16 @@ class app(QWidget):
         self.title = "Holy Math"
         self.width = 640
         self.height = 500
+        self.initUI()
     
     def initUI(self):
         FULL_TITLE = self.title + " - " + self.version
         self.setWindowTitle(FULL_TITLE)
         self.setGeometry(0,0,self.width,self.height)
+        self.show()
         
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
+    
---
+++ math/GRID.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout
+from PyQt5.QtGui import QIcon
+from pyqt
+
+def DRAW_GRID(self):
+    self.c
